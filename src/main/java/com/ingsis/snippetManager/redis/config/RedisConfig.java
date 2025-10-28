b0044bdf941abc423fe7bf0507ee2d7ba5c24a9d
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-10-28 19:01:22 -0300

    parameterize Redis connection settings using environment variables

--- src/main/java/com/ingsis/snippetManager/redis/config/RedisConfig.java
+++ src/main/java/com/ingsis/snippetManager/redis/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.ingsis.snippetManager.redis.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,9 +12,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port:6378}")
+    private int redisPort;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
