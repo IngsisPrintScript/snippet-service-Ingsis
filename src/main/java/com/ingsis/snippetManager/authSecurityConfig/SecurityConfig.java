57eb0effd981a15bb032e6a3e47d247e911015a7
Santino2005 <santinocolombo13@gmail.com>
2025-12-07 16:43:13 -0300

    Try without jwt

--- src/main/java/com/ingsis/snippetManager/authSecurityConfig/SecurityConfig.java
+++ src/main/java/com/ingsis/snippetManager/authSecurityConfig/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated()).cors(withDefaults())
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults())).csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
@@ -45,7 +46,7 @@ public class SecurityConfig {
                 "http://127.0.0.1:5173", "https://snippet-dev.duckdns.org", "https://snippet-prod.duckdns.org"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
         configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
