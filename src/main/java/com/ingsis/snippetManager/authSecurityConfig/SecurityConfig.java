3aab6704c46e2b679e76433f6cf171e3ea32cb86
Santino2005 <santinocolombo13@gmail.com>
2025-12-07 12:47:31 -0300

    Added in the config the dev url

--- src/main/java/com/ingsis/snippetManager/authSecurityConfig/SecurityConfig.java
+++ src/main/java/com/ingsis/snippetManager/authSecurityConfig/SecurityConfig.java
@@ -41,8 +41,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(
-                Arrays.asList("http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173","https://snippet-dev.duckdns.org" ));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000",
+                "http://127.0.0.1:5173","https://snippet-dev.duckdns.org","https://snippet-prod.duckdns.org"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
